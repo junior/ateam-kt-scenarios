f09983966616dc7b4af357b772b53d177f180a89
Adao Junior <junior@users.noreply.github.com>
2020-03-12 18:52:12 -0500

    changes

--- mushop-deploy/intro-course-data.sh
+++ mushop-deploy/intro-course-data.sh
@@ -1,1 +1,20 @@
+#!/bin/sh
+
 export KUBECONFIG=/root/.kube/config
+
+# Helm 3 Setup
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+
+# Setup dashboard on port 30000
+helm install dash stable/kubernetes-dashboard \
+--namespace kube-system \
+--set=service.type=NodePort \
+--set=enableInsecureLogin=true \
+--set=service.nodePort=30000 \
+--set=service.externalPort=80
+
+source <(kubectl completion bash)
+source <(helm completion bash)
+
+{ clear && echo 'Kubernetes with Helm is ready.'; } 2> /dev/null
