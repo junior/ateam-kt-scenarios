1fddf7b7b0f8f31e8cd9f98d2eece1e1fde8b0e0
Adao Junior <junior@users.noreply.github.com>
2020-03-13 15:55:42 -0500

    fix

--- oci-client/step1-code.sh
+++ oci-client/step1-code.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-oci setup autocomplete
-[[ -e "/usr/local/lib/python3.5/dist-packages/oci_cli/bin/oci_autocomplete.sh" ]] && source "/usr/local/lib/python3.5/dist-packages/oci_cli/bin/oci_autocomplete.sh"
+# oci setup autocomplete
+# [[ -e "/usr/local/lib/python3.5/dist-packages/oci_cli/bin/oci_autocomplete.sh" ]] && source "/usr/local/lib/python3.5/dist-packages/oci_cli/bin/oci_autocomplete.sh"
