3e5f1b9f5bd70ffd4bf853e66c52926a5f1c6c0c
Adao Junior <junior@users.noreply.github.com>
2020-03-13 20:30:26 -0500

    test 2

--- mushop-deploy/step6.md
+++ mushop-deploy/step6.md
@@ -1,5 +1,9 @@
 # Open MuShop
 
-On the Dashboards Tab, click on | MuShop |
+Open the deployed MuShop App here:
 
-That tab will direct you to the storefront of the MuShop App that you just deployed.
+https://[[CLIENT_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com/
+
+Or use the Dashboards Tab, click on | MuShop |
+
+That options will direct you to the storefront of the MuShop App that you just deployed.
--- oci-client/course-data.sh
+++ oci-client/course-data.sh
@@ -6,6 +6,8 @@ add-apt-repository ppa:deadsnakes/ppa -y
 apt install python3.7 -y
 mkdir /oci-cli
 cd /oci-cli
-wget -qO- -O oci-cli.zip "https://github.com/oracle/oci-cli/releases/download/v2.9.5/oci-cli-2.9.5.zip"
+wget -qO- -O oci-cli.zip "https://github.com/oracle/oci-cli/releases/download/v2.9.6/oci-cli-2.9.6.zip"
 unzip oci-cli.zip -d .. > /dev/null
 pip3 install oci_cli-*-py2.py3-none-any.whl
+yes | oci setup autocomplete
+[[ -e "/usr/local/lib/python3.5/dist-packages/oci_cli/bin/oci_autocomplete.sh" ]] && source "/usr/local/lib/python3.5/dist-packages/oci_cli/bin/oci_autocomplete.sh"
