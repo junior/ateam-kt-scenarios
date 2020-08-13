82afbc3ada762151119da88d7144abf1056f41bb
Adao Junior <junior@users.noreply.github.com>
2020-08-13 09:23:14 -0500

    updated oci-cli version

--- oci-client/background.sh
+++ oci-client/background.sh
@@ -2,11 +2,11 @@
 apt update -y
 apt install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa -y
-apt install python3.7 -y
+apt install python3.8 -y
 echo "done" >> /root/katacoda-finished
 mkdir /oci-cli
 cd /oci-cli
-wget -qO- -O oci-cli.zip "https://github.com/oracle/oci-cli/releases/download/v2.10.3/oci-cli-2.10.3.zip"
+wget -qO- -O oci-cli.zip "https://github.com/oracle/oci-cli/releases/download/v2.12.7/oci-cli-2.12.7.zip"
 unzip oci-cli.zip -d .. > /dev/null
 pip3 install oci_cli-*-py2.py3-none-any.whl
 yes | oci setup autocomplete
