5342ab2adc4193256f2c0e5efbac29371d00a0ac
Adao Junior <junior@users.noreply.github.com>
2021-03-12 15:32:46 -0600

    oci-cli progress bar

--- oci-client/foreground.sh
+++ oci-client/foreground.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-sleep 1; wait.sh
+echo -ne '#####                     (33%)\r'
+sleep 3
+echo -ne '#############             (66%)\r'
+sleep 2
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+sleep 1; wait.sh &> /dev/null
