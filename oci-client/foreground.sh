4d177c2985d1d6e53c440c3929b24275bd63b759
Adao Junior <junior@users.noreply.github.com>
2021-05-14 20:29:35 -0500

    update ingress port

--- oci-client/foreground.sh
+++ oci-client/foreground.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-echo -ne '#####                     (33%)\r'
-sleep 3
-echo -ne '#############             (66%)\r'
-sleep 2
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
-sleep 1; wait.sh &> /dev/null
+sleep 5; wait.sh
